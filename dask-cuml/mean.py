7be62889d15eab07242bb8508e054c6cc0921435
Corey J. Nolet <cjnolet@gmail.com>
2018-12-12 11:28:30 -0500

    Hard-coding chunksize for now- this will be removed when cudf is fixed

--- dask-cuml/mean.py
+++ dask-cuml/mean.py
@@ -1,16 +1,16 @@
 import dask_cudf
-from cuML import Mean
+from cuML import Mean as cumlMean
 import numpy as np
 import cudf
 
 # TODO: Let's not rely totally on dask-cudf... 
 # if they only want to use our dask_cuml.Array, 
 # they shouldn't need the other as a dependency.
-class NewMean(object):
+class Mean(object):
     def calculate(self, dask_df):
       # TODO: Should data be verified float?
       def calc_mean(df): 
-        m = Mean()
+        m = cumlMean()
         mu = m.calculate(df)
         return cudf.DataFrame([("mean", mu), ("col", range(0, len(df.columns.tolist())))])
       mu_df = dask_df.map_partitions(calc_mean)
@@ -19,6 +19,6 @@ class NewMean(object):
       # Groupby->apply functionality, this can all be done in the dask layer. 
       
       # Once cuml performs the harder work of calculating the mean for each 
-      # partition, we only have npartitions number of reductions to perform
-      # on ncols number of columns.
-      return dask_cudf.from_cudf(mu_df.compute().groupby("col").mean())
+      # partition, we only have 'npartitions' number of reductions to perform
+      # on 'ncols' number of rows.
+      return dask_cudf.from_cudf(mu_df.compute().groupby("col").mean(), chunksize = 500)
