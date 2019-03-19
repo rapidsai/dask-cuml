5998a427817f7ba1fed42cd212d83a5e68dd2550
Dillon Cullinan <dcullinan92@gmail.com>
2019-03-18 23:34:12 -0400

    Remove CUDA_REL env variable

--- ci/cpu/dask-cuml/build_dask_cuml.sh
+++ ci/cpu/dask-cuml/build_dask_cuml.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "Building dask-cuml"
 
-conda build conda/recipes/dask-cuml -c conda-forge -c numba -c rapidsai/label/cuda${CUDA_REL} -c nvidia/label/cuda${CUDA_REL} -c pytorch -c defaults --python=${PYTHON}
+conda build conda/recipes/dask-cuml -c conda-forge -c numba -c rapidsai/label/cuda${CUDA} -c nvidia/label/cuda${CUDA} -c pytorch -c defaults --python=${PYTHON}
