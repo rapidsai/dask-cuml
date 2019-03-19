d4cdaca274cb8abac5be218556d42e6f75f8ed75
Dillon Cullinan <dcullinan92@gmail.com>
2019-03-18 23:23:58 -0400

    debugging...

--- ci/cpu/dask-cuml/build_dask_cuml.sh
+++ ci/cpu/dask-cuml/build_dask_cuml.sh
@@ -4,5 +4,7 @@ set -e
 
 echo "Building dask-cuml"
 CUDA_REL=${CUDA%.*}
+echo $CUDA
+echo $CUDA_REL
 
 conda build conda/recipes/dask-cuml -c conda-forge -c numba -c rapidsai/label/cuda${CUDA_REL} -c nvidia/label/cuda${CUDA_REL} -c pytorch -c defaults --python=${PYTHON}
