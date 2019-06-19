804a8ce69f8039206e68b9990bc6411fde0e54f1
Corey J. Nolet <cjnolet@gmail.com>
2019-06-19 14:35:13 -0700

    Explicitly specify 0.8 verison for CI since we are about to merge repos for 0.9

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -32,7 +32,7 @@ logger "Activate conda env..."
 source activate gdf
 
 logger "conda install -c nvidia/label/cuda$CUDA_REL -c rapidsai/label/cuda$CUDA_REL -c conda-forge -c defaults -c rapidsai-nightly/label/cuda$CUDA_REL cuml dask distributed cudf dask-cudf dask-cuda"
-conda install -c rapidsai-nightly/label/cuda$CUDA_REL -c conda-forge -c defaults cuml dask distributed cudf dask-cudf dask-cuda
+conda install -c rapidsai-nightly/label/cuda$CUDA_REL -c conda-forge -c defaults cuml==0.8 dask distributed cudf dask-cudf dask-cuda
 
 logger "Check versions..."
 python --version
