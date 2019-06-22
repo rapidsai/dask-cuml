ea7c09edfc1540d565e7d392d2a56aa8e20bafe6
Corey J. Nolet <cjnolet@gmail.com>
2019-06-22 00:14:02 -0700

    Updating dask-cuda version to get CI passing

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -32,7 +32,7 @@ logger "Activate conda env..."
 source activate gdf
 
 logger "conda install -c nvidia/label/cuda$CUDA_REL -c rapidsai/label/cuda$CUDA_REL -c conda-forge -c defaults -c rapidsai-nightly/label/cuda$CUDA_REL cuml dask distributed cudf dask-cudf dask-cuda"
-conda install -c rapidsai-nightly/label/cuda$CUDA_REL -c conda-forge -c defaults cuml=0.8.0 dask distributed cudf=0.8.0 dask-cudf dask-cuda
+conda install -c rapidsai-nightly/label/cuda$CUDA_REL -c conda-forge -c defaults cuml=0.8.0 dask distributed cudf=0.8.0 dask-cudf dask-cuda=0.7
 
 logger "Check versions..."
 python --version
